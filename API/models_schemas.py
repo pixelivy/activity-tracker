880ed22172c2d3aa93a7e09f318a0b2725e67120
Skye Jonczik <jeanjonczik2003@gmail.com>
2026-05-29 16:25:06 +0200

    refactor: encode filter steps, add SQL Model and base Schemas

---
+++ API/models_schemas.py
@@ -0,0 +1,63 @@
+from sqlmodel import SQLModel, Field
+from sqlalchemy import CheckConstraint
+from datetime import datetime
+from typing import Optional
+
+
+class Activity(SQLModel, table=True):
+    __tablename__ = "activities"
+    __table_args__ = (
+        CheckConstraint("capacities IN ('1', '2', '3')", name="check_capacities"),
+        CheckConstraint("cost IN ('0', '1', '2', '3')", name="check_cost"),
+        CheckConstraint("distance IN ('0', '1', '2', '3')", name="check_distance"),
+        CheckConstraint("time IN ('1', '2', '3')", name="check_time")
+    )
+
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(nullable=False)
+    capacities: str
+    cost: str
+    distance: str
+    time: str
+    counter: int = Field(default=0)
+    description: Optional[str] = None
+    url: Optional[str] = None
+    creation_time: datetime = Field(default_factory=datetime.utcnow)
+    update_time: datetime = Field(default_factory=datetime.utcnow)
+
+class createActivity(SQLModel):
+    name: str
+    capacities: str
+    cost: str
+    distance: str
+    time: str
+    description: str
+    url: str
+
+
+class updateActivity(SQLModel):
+    description: str
+    url: str
+
+class deleteActivity(SQLModel):
+    id: int
+    deleted: bool
+    delete_time: datetime
+
+class getActivity(SQLModel):
+    name: str
+    capacities: str
+    cost: str
+    distance: str
+    time: str
+    counter: int
+    description: str
+    url: str
+
+
+class getRecommendation(SQLModel):
+    capacities: str
+    cost: str
+    distance: str
+    time: str
