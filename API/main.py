e8df16da1ec5a9036cc7c98621defd6544bd7ee6
SkyeJxn <78538146+SkyeJxn@users.noreply.github.com>
2026-05-30 16:02:55 +0200

    refactor: add error handling for activity retrieval and deletion, update activity response message

--- API/main.py
+++ API/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from sqlmodel import select
 from API.models_schemas import getRecommendation, getActivity, createActivity, updateActivity, Activity
 from API.database import SessionDep
 
@@ -24,24 +25,45 @@ def chooseActivity(id: int, session: SessionDep):
 
 @app.patch("/update_activity")
 def updateActivity(input: updateActivity, session: SessionDep):
-    activity = Activity.model_validate(input)
+    activity = session.get(Activity, input.id)
+    if not activity:
+        raise HTTPException(status_code=404, detail="Activity not found")
+    activity.description = input.description
+    activity.url = input.url
     session.add(activity)
     session.commit()
-    return {"status": "200", "message": "Activity created"}
+    return {"status": "200", "message": "Activity updated"}
 
 @app.delete("/delete_activity")
-def addActivity():
-    return None
+def addActivity(id: int, session: SessionDep):
+    result = session.get(Activity, id)
+    if not result:
+        raise HTTPException(404, "Activity not found")
+    session.delete(result)
+    session.commit()
+    return {"status": "200", "message": "Activity deleted"}
 
-@app.get("/get_activity/")
-def getActivity(id: int):
+@app.get("/get_activity/", response_model=getActivity)
+def getDescription(id: int, session: SessionDep):
+    result = session.get(Activity, id)
+    if not result:
+        raise HTTPException(404, "Activity not found")
+    return result
 
-    return {}
+@app.get("/get_activities", response_model=list[getActivity])
+def getActivities(session: SessionDep) -> list[Activity]:
+    result = session.exec(select(Activity)).all()
+    if not result:
+        raise HTTPException(404, "Activity not found")
+    return result
 
-@app.get("/get_activities")
-def getActivities():
-    return None
-
-@app.get("/get_recommendations")
-def getRecommendations():
-    return None
+@app.get("/get_recommendations", response_model=list[getActivity])
+def getRecommendations(capacities: int, cost: int, time: int, distance: int, friends: bool, session: SessionDep):
+    if friends:
+        statement = select(Activity).where(Activity.capacities <= capacities).where(Activity.cost <= cost).where(Activity.time <= time).where(Activity.distance <= distance).where(Activity.friends == True)
+    else:
+        statement = select(Activity).where(Activity.capacities <= capacities).where(Activity.cost <= cost).where(Activity.time <= time).where(Activity.distance <= distance)
+    result = session.exec(statement)
+    if not result:
+        raise HTTPException(404, "no fitting Activity")
+    return result.all()
