bbe516a228eddc8c7a7fdd634152fefa465adb64
SkyeJxn <78538146+SkyeJxn@users.noreply.github.com>
2026-05-31 21:41:45 +0200

    chore: change UI Library
    - removed PrimeReact and imports
    - added base UI

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -3,17 +3,14 @@ import Home from "./routes/Home.tsx";
 import Create from "./routes/Create.tsx";
 import Listing from "./routes/Overview.tsx";
 import Missing from "./routes/ErrorPage.tsx";
-import {PrimeReactProvider} from 'primereact/api';
 
 export default function App(){
   return (
-    <PrimeReactProvider value={{ unstyled: true }}>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/create" element={<Create/>}/>
         <Route path="/overview" element={<Listing/>}/>
         <Route path="*" element={<Missing/>}/>
       </Routes>
-    </PrimeReactProvider>
   );
 }
