e8aa96fa5bb8b9bf2c6d394039b61e04ff254b81
Skye Jonczik <jeanjonczik2003@gmail.com>
2026-06-26 08:59:17 +0200

    feat: add Header and Footer components for improved layout in App

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -3,18 +3,21 @@ import Home from "./routes/Home.tsx";
 import Create from "./routes/Create.tsx";
 import Listing from "./routes/Overview.tsx";
 import Missing from "./routes/ErrorPage.tsx";
+import {Header, Footer} from "./components/layout.tsx"
 
 export default function App(){
   return (
-    <div className="bg-back h-screen w-screen items-center justify-center justify-items-center">
-      <div className="bg-surface size-[80%] m-10 p-10 justify-content-center align-items-center absolute broder-edge rounded-2xl">
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/create" element={<Create/>}/>
-        <Route path="/overview" element={<Listing/>}/>
-        <Route path="*" element={<Missing/>}/>
-      </Routes>
+    <div className="bg-back absolute flex h-screen w-screen items-center justify-center justify-items-center">
+      <Header/>
+      <div className="bg-surface relative size-[80%] m-10 p-10 justify-content-center border-edge border-2 align-items-center rounded-2xl">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/create" element={<Create/>}/>
+          <Route path="/overview" element={<Listing/>}/>
+          <Route path="*" element={<Missing/>}/>
+        </Routes>
       </div>
+      <Footer/>
     </div>
   );
 }
---
+++ UI/src/components/layout.tsx
@@ -0,0 +1,13 @@
+export function Header(){
+    return (
+    <div className="w-full h-11 bg-surface absolute border-b-edge border-b-2 top-0 mb-8">
+        <label>Activity Tracker</label>
+    </div>)
+}
+
+export function Footer(){
+    return (
+    <div className="w-full h-11 bg-surface border-t-edge border-t-2 absolute bottom-0">
+        <label></label>
+    </div>)
+}
