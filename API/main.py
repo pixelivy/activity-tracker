8a757c90cbcb4aa2386e80cc52f0dd83b198c03c
Skye Jonczik <jeanjonczik2003@gmail.com>
2026-06-28 13:21:58 +0200

    feat: refactor activity handling and update model schemas for consistency

--- API/main.py
+++ API/main.py
@@ -1,69 +1,80 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import select
-from API.models_schemas import getActivity, createActivity, updateActivity, Activity
+from collections.abc import Sequence
+from API.models_schemas import Activities, getActivity, createActivity, ActivityID
 from API.database import SessionDep
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.post("/add_activity")
-def addActivity(input: createActivity, session: SessionDep):
-    activity = Activity.model_validate(input.model_dump())
+def add_an_activity(input: createActivity, session: SessionDep):
+    activity = Activities.model_validate(input.model_dump())
     session.add(activity)
     session.commit()
-    return {"status": "200", "message": "Activity created"}
+    return {"status": "200", "message": "Activities created"}
 
 @app.patch("/choose_activity")
-def chooseActivity(id: int, session: SessionDep):
-    activity = session.get(Activity, id)
+def choose_an_activity(id: int, session: SessionDep):
+    activity = session.get(Activities, id)
     if not activity:
-        raise HTTPException(status_code=404, detail="Activity not found")
+        raise HTTPException(status_code=404, detail="Activities not found")
     update = {"counter": int(activity.counter) + 1}
     activity.sqlmodel_update(update)
     session.add(activity)
     session.commit()
-    return {"status": "200", "message": "Activity created"}
+    return {"status": "200", "message": "Activities created"}
 
 @app.patch("/update_activity")
-def updateActivity(input: updateActivity, session: SessionDep):
-    activity = session.get(Activity, input.id)
+def update_an__activity(input: ActivityID, session: SessionDep):
+    activity = session.get(Activities, input.id)
     if not activity:
-        raise HTTPException(status_code=404, detail="Activity not found")
+        raise HTTPException(status_code=404, detail="Activities not found")
     activity.description = input.description
     activity.url = input.url
     session.add(activity)
     session.commit()
-    return {"status": "200", "message": "Activity updated"}
+    return {"status": "200", "message": "Activities updated"}
 
 @app.delete("/delete_activity")
-def addActivity(id: int, session: SessionDep):
-    result = session.get(Activity, id)
+def delete_an_activity(id: int, session: SessionDep):
+    result = session.get(Activities, id)
     if not result:
-        raise HTTPException(404, "Activity not found")
+        raise HTTPException(404, "Activities not found")
     session.delete(result)
     session.commit()
-    return {"status": "200", "message": "Activity deleted"}
+    return {"status": "200", "message": "Activities deleted"}
 
-@app.get("/get_activity/", response_model=getActivity)
-def getDescription(id: int, session: SessionDep):
-    result = session.get(Activity, id)
+@app.get("/get_activity", response_model=getActivity)
+def get_activity_Description(id: int, session: SessionDep):
+    result = session.get(Activities, id)
     if not result:
-        raise HTTPException(404, "Activity not found")
+        raise HTTPException(404, "Activities not found")
     return result
 
-@app.get("/get_activities", response_model=list[getActivity])
-def getActivities(session: SessionDep) -> list[Activity]:
-    result = session.exec(select(Activity)).all()
+@app.get("/get_activities", response_model=Sequence[ActivityID])
+def get_all_activities(session: SessionDep) -> Sequence[ActivityID]:
+    result = session.exec(select(Activities)).all()
     if not result:
-        raise HTTPException(404, "Activity not found")
+        raise HTTPException(404, "Activities not found")
+    # FIX: return type error
     return result
 
 @app.get("/get_recommendations", response_model=list[getActivity])
-def getRecommendations(capacities: int, cost: int, time: int, distance: int, friends: bool, session: SessionDep):
+def get_a_list_of_recommendations(capacities: int, cost: int, time: int, distance: int, friends: bool, session: SessionDep):
     if friends:
-        statement = select(Activity).where(Activity.capacities <= capacities).where(Activity.cost <= cost).where(Activity.time <= time).where(Activity.distance <= distance).where(Activity.friends == True)
+        statement = select(Activities).where(Activities.capacities <= capacities).where(Activities.cost <= cost).where(Activities.time <= time).where(Activities.distance <= distance).where(Activities.friends == True)
     else:
-        statement = select(Activity).where(Activity.capacities <= capacities).where(Activity.cost <= cost).where(Activity.time <= time).where(Activity.distance <= distance)
+        statement = select(Activities).where(Activities.capacities <= capacities).where(Activities.cost <= cost).where(Activities.time <= time).where(Activities.distance <= distance)
     result = session.exec(statement)
     if not result:
-        raise HTTPException(404, "no fitting Activity")
+        raise HTTPException(404, "no fitting Activities")
     return result.all()
--- API/models_schemas.py
+++ API/models_schemas.py
@@ -3,9 +3,12 @@ from sqlalchemy import CheckConstraint
 from datetime import datetime
 from typing import Optional
 
+class BaseActivity(SQLModel):
+    name: str = Field(nullable=False)
+    description: str
+    url: str
 
-class Activity(SQLModel, table=True):
-    __tablename__ = "activities"
+class Activities(BaseActivity, table=True):
     __table_args__ = (
         CheckConstraint("capacities IN (1, 2, 3)", name="check_capacities"),
         CheckConstraint("cost IN (0, 1, 2, 3)", name="check_cost"),
@@ -15,39 +18,29 @@ class Activity(SQLModel, table=True):
 
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str = Field(nullable=False)
     capacities: int
     cost: int
     distance: int
     time: int
     friends: bool
     counter: int = Field(default=0)
-    description: Optional[str] = None
-    url: Optional[str] = None
     creation_time: datetime = Field(default_factory=datetime.utcnow)
     update_time: datetime = Field(default_factory=datetime.utcnow)
 
-class createActivity(SQLModel):
-    name: str
+class createActivity(BaseActivity):
     capacities: int = Field(default=2)
     cost: int = Field(default=2)
     distance: int = Field(default=2)
     time: int = Field(default=2)
     friends: bool
-    description: str
-    url: str
 
-class updateActivity(SQLModel):
+class ActivityID(BaseActivity):
     id: int
-    description: str
-    url: str
 
-class getActivity(SQLModel):
-    name: str
-    capacities: int
+class getActivity(BaseActivity):
+    id: int
     cost: int
     distance: int
     time: int
     counter: int
     description: str
-    url: str
